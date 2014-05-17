8cc61c311779adee8f9342e54fc8155ce4d82ce8
Kristjan Rang <mail@rang.ee>
2014-05-17 13:53:58 +0300

    Use google DNS

--- stack/build.sh
+++ stack/build.sh
@@ -4,6 +4,11 @@ exec 2>&1
 set -e
 set -x
 
+cat > /etc/resolv.conf <<EOF
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+
 cat > /etc/apt/sources.list <<EOF
 deb http://archive.ubuntu.com/ubuntu trusty main
 deb http://archive.ubuntu.com/ubuntu trusty-security main
